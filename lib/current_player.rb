e54fdedeb91eb49202c58977b190a9b6385cf21c
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-20 13:39:19 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,16 @@
+def turn_count(board)
+  counter = 0
+  board.each { |element|
+  if (element == "X" || element == "O")
+    counter += 1
+  end
+end
+
+def current_player(board)
+  counter = turn_count(board)
+  if (counter%2 == 0)
+     "X"
+  elsif (counter%2 != 0)
+     "O"
+  end
+end
